5c2eac33c1303573567e4e65b935602fa6c4235f
nathanAdmisys <92980871+nathanAdmisys@users.noreply.github.com>
2023-10-26 11:37:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,6 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route("2ème route/")
+def hello_world(je suis la deuxième route):
+    return "<p>Hello, route2!</p>"
